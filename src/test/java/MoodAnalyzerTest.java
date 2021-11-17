5204285f88f404dc7b8e944af272fb2e12a1694c
mudable <mudableshilpa@gmail.com>
2021-11-17 07:37:44 +0530

    Added : UC3 to cehck for invalid input

--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -3,26 +3,31 @@ import static org.junit.Assert.*;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.bridgelabz.moodanalyzer.MoodAnalysisException;
 import com.bridgelabz.moodanalyzer.MoodAnalyzer;
 
 /*Added test cases to check the haapy and sad mood*/
 public class MoodAnalyzerTest {
-	@Test
-	public void givenMessage_SadMood_Should_Return_Sad() {
-		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am In a Sad Mood");
-		String mood = moodAnalyzer.analyseMood();
-		Assert.assertEquals("SAD", mood);
-	}
-	@Test
-	public void givenMessage_SadMood_Should_Return_Happy() {
-		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am In a Sad Mood");
-		String mood = moodAnalyzer.analyseMood();
-		Assert.assertEquals("HAPPY", mood);
-	}
-	@Test
-	public void givenMessage_AnyMood_Should_HandleException() {
-		MoodAnalyzer moodAnalyser = new MoodAnalyzer(null);
-		String mood = moodAnalyser.analyseMood();
-		Assert.assertEquals("HAPPY", mood);
-	}
+    @Test
+    public void givenMessage_SadMood_Should_Return_Sad() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am In a Sad Mood");
+        String mood;
+        try {
+            mood = moodAnalyzer.analyseMood();
+            Assert.assertEquals("SAD", mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
+}
+    @Test
+    public void givenMessage_AnyMood_Should_Return_HAPPY() {
+        MoodAnalyzer moodAnalyser = new MoodAnalyzer("I am in Happy Mood");
+        String mood;
+        try {
+            mood = moodAnalyser.analyseMood();
+            Assert.assertEquals("HAPPY", mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
+    }
 }
